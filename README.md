b237dcd132a7c78bcb6334efa518fca6bf02e888
lioujj <34760803+lioujj@users.noreply.github.com>
2018-02-03 22:22:00 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pxt-sensors
+# sensors
+
+這是專為micro:bit提供的感應器積木
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+sensors=github:lioujj/pxt-sensors
+```
---
+++ main.ts
@@ -0,0 +1,33 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="sensors"
+namespace sensors {
+
+    export enum PingUnit {
+        //% blockId=unit_cm block="cm"
+        Centimeters,
+        //% blockId=unit_inch block="inches"
+        Inches,
+        //% blockId=unit_us block="μs"
+        MicroSeconds
+    }
+
+
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return d / 58;
+            case PingUnit.Inches: return d / 148;
+            default: return d ;
+        }
+    }
+}
